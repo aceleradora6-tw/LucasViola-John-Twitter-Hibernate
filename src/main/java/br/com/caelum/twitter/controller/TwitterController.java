88640d9c8c12ffe54503fc5bb036aff2051cbbd7
jonatassalgado <jonatas.salgado@acad.pucrs.br>
2014-10-27 10:26:05 -0200

    Hibernate configurado

---
+++ src/main/java/br/com/caelum/twitter/controller/TwitterController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.twitter.controller;
+
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Resource;
+
+
+@Resource
+public class TwitterController {
+	
+	@Get("/newsfeed")
+	public void newsfeed() {
+		
+	}
+
+}
---
+++ src/main/java/br/com/caelum/twitter/model/Tweet.java
@@ -0,0 +1,9 @@
+package br.com.caelum.twitter.model;
+
+public class Tweet {
+	
+	private int id;
+	private String mensagem;
+	
+
+}
